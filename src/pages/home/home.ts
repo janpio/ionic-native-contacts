903007e3af8e4d27815663f394bcb62950dc0634
Jan Piotrowski <piotrowski@gmail.com>
2017-07-07 10:55:23 +0200

    contactList

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,12 +11,26 @@ export class HomePage {
 
   }
 
+  contactList = [];
+
   getContacts(): void {
     this.contacts.find(
       ["displayName", "phoneNumbers","photos"],
       {multiple: true, hasPhoneNumber: true}
       ).then((contacts) => {
-        console.log(contacts);
+        for (var i=0 ; i < contacts.length; i++){
+          if(contacts[i].displayName !== null) {
+            var contact = {};
+            contact["name"]   = contacts[i].displayName;
+            contact["number"] = contacts[i].phoneNumbers[0].value;
+            if(contacts[i].photos != null) {
+              console.log(contacts[i].photos);
+              contact["image"]  = contacts[i].photos[0].value;
+              console.log(contact);
+              this.contactList.push(contact);
+            }
+          }
+        }
     });
   }
 
